8a381d8c4070bd87ac550b3d01defd7f9928f8b9
Giuseppe Silletti <sillettig@gmail.com>
2015-10-02 12:16:04 +0200

    Aggiunta la dipendenza per RiakClient in Maven

--- coffiURL/src/main/java/com/silletti/coffiURL/App.java
+++ coffiURL/src/main/java/com/silletti/coffiURL/App.java
@@ -1,5 +1,9 @@
 package com.silletti.coffiURL;
 
+import java.net.UnknownHostException;
+
+import com.basho.riak.client.api.RiakClient;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
     }
 }
